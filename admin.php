0a1399af4ba53f1d61be40be50887f9d4fdcb39b
Bui Tam <buitam2708@gmail.com>
2019-06-02 22:47:07 +0700

    lala

--- admin.php
+++ admin.php
@@ -36,7 +36,7 @@ session_start();?>
     <div class="carousel-inner">
 
       <div class="item active">
-        <img src="la.jpg" alt="Los Angeles" style="width:100%;">
+        <img src="img/la.jpg" alt="Los Angeles" style="width:100%;">
         <div class="carousel-caption">
           <h1>FPT SOFTWARE</h1>
           <p>Your time is limited, so don't waste it living someone else's life</p>
@@ -44,7 +44,7 @@ session_start();?>
       </div>
 
       <div class="item">
-        <img src="chicago.jpg" alt="Chicago" style="width:100%;">
+        <img src="img/chicago.jpg" alt="Chicago" style="width:100%;">
         <div class="carousel-caption">
           <h1>ARTIFICIAL INTELLIGENCE</h1>
           <p>AI has become an essential part of the technology industry</p>
@@ -52,7 +52,7 @@ session_start();?>
       </div>
     
       <div class="item">
-        <img src="ny.jpg" alt="New York" style="width:100%;">
+        <img src="img/ny.jpg" alt="New York" style="width:100%;">
         <div class="carousel-caption">
           <h1>GREENWICH UNIVERSITY</h1>
           <p>Wisdom is not a product from school, but a lifelong learning process</p>
