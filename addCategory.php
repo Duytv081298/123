a479f2b54b261db8932d3dd719900aef8fbff67b
Bui Tam <buitam2708@gmail.com>
2019-06-05 11:43:19 +0700

    kaka

--- addCategory.php
+++ addCategory.php
@@ -1,20 +1,20 @@
 
 <?php
   // Create database connection
-$db = mysqli_connect("localhost", "root", "", "website");
+  $db = mysqli_connect("localhost", "root", "", "website");
 
   // If upload button is clicked ...
-if (isset($_POST['upload'])) {
+  if (isset($_POST['upload'])) {
     // Get text
-  $name = mysqli_real_escape_string($db, $_POST['name']);
-  $description = mysqli_real_escape_string($db, $_POST['description']);
-  $idstaff = mysqli_real_escape_string($db, $_POST['idstaff']);
+    $name = mysqli_real_escape_string($db, $_POST['name']);
+    $description = mysqli_real_escape_string($db, $_POST['description']);
+    $idstaff = mysqli_real_escape_string($db, $_POST['idstaff']);
 
-  $sql = "INSERT INTO  category (name, description, idstaff) VALUES ('$name', '$description', '$idstaff')";
+    $sql = "INSERT INTO  category (name, description, idstaff) VALUES ('$name', '$description', '$idstaff')";
     // execute query
-  mysqli_query($db, $sql);
-
-}
+    mysqli_query($db, $sql);
+    
+  }
 
 ?>
 
@@ -22,40 +22,46 @@ if (isset($_POST['upload'])) {
 <html>
 <link rel="stylesheet" type="text/css" href="css/styleadd.css">
 <body>
-	<div class="content" style="padding: 50px 200px 50px 200px">
+  <div class="content" style="padding: 50px 200px 50px 200px">
 
-    <form action="trainingStaff.php" style="border:1px solid #ccc" method="POST" enctype="multipart/form-data">
-      <div class="container">
-        <h1>Add Category</h1>
-        <p>Please fill in this form to add Category.</p>
-        <hr>
-
-
-        <label for="name"><b>Category's name</b></label> 
-        <input type="text" placeholder="Enter category's name" name="name" required>
-
-        <label for="description"><b>Category's Description </b></label>
-        <input type="text" placeholder="Enter category's Description" name="description" required>
-        <label for="idstaff"><b>ID Staff </b></label>
-        <input type="text" placeholder="Enter id staff" name="idstaff" required>
+<form action="addCategory.php" style="border:1px solid #ccc" method="POST" enctype="multipart/form-data">
+  <div class="container">
+    <h1>Add Category</h1>
+    <p>Please fill in this form to add Category.</p>
+    <hr>
 
 
-        <form action="/action_page.php" method="get">
-          <input list="browsers" name="browser">
-          <datalist id="browsers">
-            <option value="Internet Explorer">
-          </datalist>
-          <input type="submit">
-        </form>
+    <label for="name"><b>Category's name</b></label> 
+    <input type="text" placeholder="Enter category's name" name="name" required>
 
+    <label for="description"><b>Category's Description </b></label>
+    <input type="text" placeholder="Enter category's Description" name="description" required>
+    <label for="idstaff"><b>ID Staff </b></label>
+    
+        <input list="idstaff" name="idstaff">
+        <datalist id="idstaff">
+        <?php 
+        require_once'db.php';
 
+        $sql = "SELECT * FROM staff ";
 
-                  <div class="clearfix">
-                    <button type="submit" name="upload">Add Category</button>
-                  </div>
-                </div>
-              </form>
-            </div>
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+                  // output data of each row
+          while($row = $result->fetch_assoc()) {
+            ?>
+            <option><?php echo $row["idstaff"]?></option>
+            <?php
+          }
+        }
+        ?>
+        </datalist>
+                   <div class="clearfix">
+      <button type="submit" name="upload">Add Category</button>
+    </div>
+  </div>
+</form>
+</div>
 
-          </body>
-          </html>
+</body>
+</html>
--- addCourse.php
+++ addCourse.php
@@ -1,10 +1,31 @@
+
+<?php
+  // Create database connection
+$db = mysqli_connect("localhost", "root", "", "website");
+
+  // If upload button is clicked ...
+if (isset($_POST['upload'])) {
+    // Get text
+  $name = mysqli_real_escape_string($db, $_POST['name']);
+  $description = mysqli_real_escape_string($db, $_POST['description']);
+  $idstaff = mysqli_real_escape_string($db, $_POST['idstaff']);
+  $idcategory = mysqli_real_escape_string($db, $_POST['idcategory']);
+
+  $sql = "INSERT INTO  course (name, description, idstaff, idcategory) VALUES ('$name', '$description', '$idstaff', '$idcategory')";
+    // execute query
+  mysqli_query($db, $sql);
+
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" type="text/css" href="css/styleadd.css">
+
 <body>
 	<div class="content" style="padding: 50px 200px 50px 200px">
 
-<form action="addCourse.php" style="border:1px solid #ccc">
+<form action="addCourse.php" style="border:1px solid #ccc" method="POST" enctype="multipart/form-data">
   <div class="container">
     <h1>Add Course</h1>
     <p>Please fill in this form to add course.</p>
@@ -14,17 +35,17 @@
     <label for="name"><b>Course's name</b></label> 
     <input type="text" placeholder="Enter Course's name" name="name" required>
 
+    <label for="description"><b>Course's Detail </b></label>
+    <input type="text" placeholder="Enter Course's Description" name="description" required>
+
+    <label for="idstaff"><b>ID Staff </b></label>
+    <input type="text" placeholder="Enter id staff" name="idstaff" required>
+    
     <label for="idcategory"><b>Course's Category</b></label>
     <input type="text" placeholder="Enter Course's Catagory" name="idcategory" required>
 
-
-    <label for="description"><b>Course's Detail </b></label>
-    <input type="text" placeholder="Enter Course's Description" name="description" required>
-    
-    
-
     <div class="clearfix">
-      <button type="submit" class="addCourse">Add Course</button>
+      <button type="submit" class="addCourse" name="upload">Add Course</button>
     </div>
   </div>
 </form>
