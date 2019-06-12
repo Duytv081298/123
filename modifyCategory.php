246b76d8ab788127825d12c97875f8bb1e938d2a
Trịnh Văn Duy <49477836+Duytv081298@users.noreply.github.com>
2019-06-12 11:27:32 +0700

    1234567

--- modifyCategory.php
+++ modifyCategory.php
@@ -4,7 +4,7 @@ session_start();
 $idstaff = $_SESSION['idstaff'];
 ?>
 
-       
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,23 +13,19 @@ $idstaff = $_SESSION['idstaff'];
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.4.0/css/perfect-scrollbar.css">
-
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-
 </head>
 <body>
-	
- 	<?php 
-		if(isset($_GET['del']))
-	   {
-	   	require_once'db.php';
-	   	$idcategory = $_GET['del'];
-	    $sql ="DELETE FROM category WHERE idcategory ='". (int)$idcategory ."'";
-	    $result = $conn-> query($sql);
-	    $message = "Category Deleted!";
-		echo "<script type='text/javascript'>alert('$message');</script>";
-	   }
-	 ?>
+	<?php 
+	if(isset($_GET['deletecategory']))
+	{
+		require_once'db.php';
+		$idcategory = $_GET['deletecategory'];
+		$sql ="DELETE FROM category WHERE idcategory ='". (int)$idcategory ."'";
+		$result = $conn-> query($sql);
+		Header( "Location: modifyCategory.php" );
+	}
+	?>  
 	<div class="limiter">
 		<div class="container-table100">
 			<div class="wrap-table100">
@@ -47,55 +43,47 @@ $idstaff = $_SESSION['idstaff'];
 							</tr>
 						</thead>
 						<tbody>
-					<?php 
+							<?php 
 							require_once'db.php';
-
 							$sql = "SELECT * FROM category ";
-
-							
-
 							$result = $conn->query($sql);
 							if ($result->num_rows > 0) {
 							    // output data of each row
-							    while($row = $result->fetch_assoc()) {
-							    	$idcategory =  $row["idcategory"];
-							    	?>
-								<tr>
-									<td class="column1"><?php echo $row["idcategory"]?></td>
-									<td class="column2"><?php echo $row["name"]?></td>
-									<td class="column3"><?php echo $row["description"]?></td>
-									<td class="column4">
-					<?php 
-							require_once'db.php';
-							$sql1 = "SELECT course.name FROM category INNER JOIN course ON category.idcategory=course.idcategory where course.idcategory =".$idcategory;
-							$result1 = $conn->query($sql1);
-							if ($result1->num_rows > 0) {
+								while($row = $result->fetch_assoc()) {
+									$_SESSION['idcategory'] = $row['idcategory'];
+									$idcategory = $_SESSION['idcategory'];
+									?>
+									<tr>
+										<td class="column1"><?php echo $row["idcategory"]?></td>
+										<td class="column2"><?php echo $row["name"]?></td>
+										<td class="column3"><?php echo $row["description"]?></td>
+										<td class="column4">
+											<?php 
+											require_once'db.php';
+											$sql1 = "SELECT course.name FROM category INNER JOIN course ON category.idcategory=course.idcategory where course.idcategory =".$idcategory;
+											$result1 = $conn->query($sql1);
+											if ($result1->num_rows > 0) {
 							    // output data of each row
-							    while($row1 = $result1->fetch_assoc()) {
-							    	?>
-								<span><?php echo $row1["name"]?></span>
+												while($row1 = $result1->fetch_assoc()) {
+													?>
+													<span><?php echo $row1["name"]?></span>
+													<?php
+												}
+											}
+											?>
+										</td>
+										<td class="column5"><button>Update</button></td>
 
-					<?php
-									}
-									}
-									?>
+										<td class="column6">
+											<a href="modifyCategory.php?deletecategory=<?php echo $row["idcategory"]?>"> <button type="Delete">Delete</button>
+											</a>
+										</td>
+									</tr>
+									<?php
+								}
+							}
+							?>
 
-
-									</td>
-									<td class="column5"><button>Update</button></td>
-
-									<td class="column6">
-										<a href="modifyCategory.php?del=<?php echo $row["idcategory"]?>"> <input type="button" value="Delete" name="deletecategory"> 
-										</a>
-
-									</td>
-									
-								</tr>
-					<?php
-									}
-									}
-									?>
-																	
 						</tbody>
 					</table>
 				</div>
