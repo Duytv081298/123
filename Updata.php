e4ea32838441036db6b1183d4897df27dc0e4716
Trịnh Văn Duy <49477836+Duytv081298@users.noreply.github.com>
2019-06-13 00:12:15 +0700

    1245513

--- Updata.php
+++ Updata.php
@@ -1,14 +1,19 @@
 <?php
 session_start();
 $idstaff = $_SESSION['idstaff'];
+$user = $_SESSION['user'];
+$pass = $_SESSION['pass'];
+$name = $_SESSION['name'];
 ?>
 
 <?php
   $db = mysqli_connect("localhost", "root", "", "website");
   if (isset($_POST['updataStaff'])) {
+    $idstaff = mysqli_real_escape_string($db, $_POST['idstaff']);
+    $user = mysqli_real_escape_string($db, $_POST['user']);
+    $pass = mysqli_real_escape_string($db, $_POST['pass']);
     $name = mysqli_real_escape_string($db, $_POST['name']);
-    $description = mysqli_real_escape_string($db, $_POST['description']);
-    $sql = "INSERT INTO  category (name, description, idstaff) VALUES ('$name', '$description', '$idstaff')";
+    $sql = "UPDATE `staff` SET `idstaff`='$idstaff',`user`='$user',`pass`='$pass',`name`='$name' WHERE `idstaff`= $idstaff";
     mysqli_query($db, $sql);
     Header( "Location: modifyCategory.php" );
 }
