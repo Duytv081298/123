5e32e3afa40a134416ee5f26f8886e0b443e550c
Bui Tam <buitam2708@gmail.com>
2019-06-14 15:29:58 +0700

    hahah

--- update.php
+++ update.php
@@ -9,7 +9,7 @@ $idstaff = $_SESSION['idstaff'];
     $user = $_POST['user'];
     $pass = $_POST['pass'];
     $name = $_POST['name'];
-    $sql = "UPDATE staff SET idstaff`='$idstaff',user`='$user',`pass`='$pass',`name`='$name'  WHERE `idstaff`= $idstaff";
+    $sql = "UPDATE `staff` SET `idstaff`='$idstaff',`user`='$user',`pass`='$pass',`name`='$name'  WHERE `idstaff`= $idstaff";
     mysqli_query($db, $sql);
     Header( "Location: TrainingStaff.php" );
 }elseif(isset($_POST['updateCourse'])) {
@@ -17,7 +17,7 @@ $idstaff = $_SESSION['idstaff'];
     $description = $_POST['description'];
     $idcategory = $_POST['idcategory'];
     $idcourse = $_POST['idcourse'];
-    $sql = "UPDATE course SET idcourse`='$idcourse',name`='$name',`description`='$description', idstaff`='$idstaff',idcategory`= '$idcategory' WHERE `idcourse`= $idcourse";
+    $sql = "UPDATE `course` SET `idcourse`='$idcourse',`name`='$name',`description`='$description', `idstaff`='$idstaff', `idcategory`= '$idcategory' WHERE `idcourse`= $idcourse";
     mysqli_query($db, $sql);
     Header( "Location: modifyCourse.php" );
 }elseif(isset($_POST['updateTopic'])) {
@@ -25,7 +25,7 @@ $idstaff = $_SESSION['idstaff'];
     $description = $_POST['description'];
     $idtopic = $_POST['idtopic'];
     $idcourse = $_POST['idcourse'];
-    $sql = "UPDATE topic SET idtopic`='$idtopic',name`='$name',`description`='$description', idstaff`='$idstaff',idcourse`= '$idcourse' WHERE `idtopic`= $idtopic";
+    $sql = "UPDATE `topic` SET `idtopic`='$idtopic',`name`='$name',`description`='$description', `idstaff`='$idstaff', `idcourse`= '$idcourse' WHERE `idtopic`= $idtopic";
     mysqli_query($db, $sql);
 
 }elseif(isset($_POST['updateTrainerAdmin'])) {
@@ -34,7 +34,7 @@ $idstaff = $_SESSION['idstaff'];
         $pass =  $_POST["pass"];
         $name =  $_POST["name"];
         $status =  $_POST["status"];
-    $sql = "UPDATE trainer SET idtrainer`='$idtrainer',name`='$name',`user`='$user', pass`='$pass',status`= '$status' WHERE `idtrainer`= $idtrainer";
+    $sql = "UPDATE `trainer` SET `idtrainer`='$idtrainer',`name`='$name',`user`='$user', `pass`='$pass', `status`= '$status' WHERE `idtrainer`= $idtrainer";
     mysqli_query($db, $sql);
 
 }
